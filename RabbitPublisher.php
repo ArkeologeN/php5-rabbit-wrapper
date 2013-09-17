22484ebf7867b6b25d5742abd1c691f518daf3f5
Hamza Waqas <hamzawaqas.programmer@gmail.com>
2013-09-17 12:08:14 +0500

    misc. files ignored

--- RabbitPublisher.php
+++ RabbitPublisher.php
@@ -80,7 +80,9 @@ class RabbitPublisher  {
                $this->_exchange->setName($this->_options['exchange.name']);
                $this->_exchange->setType($this->_options['exchange.type']);
                $this->_exchange->setFlags($this->_options['exchange.flag']);
-               $this->_exchange->declare();
+               //$this->_exchange->declare();
+               $this->_exchange->declareExchange();
+
            }
        } catch (\Exception $ex) {
            echo "<pre>"; print_r($ex); exit;
